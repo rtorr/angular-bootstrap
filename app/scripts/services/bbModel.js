cf4032f5133b61ef7bdfb66d208b4e6780177d4f
Richard Torruellas <rtorruellas@gmail.com>
2013-12-06 23:09:29 -0700

    trying stupid stuff

---
+++ app/scripts/services/bbModel.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/*global app */
+
+
+app.service('bbModel', function () {
+
+  var Person = Backbone.Model.extend({
+
+    defaults: {
+      "name":  "",
+      "age": ""
+    }
+
+  });
+
+  this.People = new Backbone.Collection([], {
+    model: Person
+  });
+
+  this.addPerson = function(name){
+    var p = new Person({name:name});
+    this.People.push(p);
+    console.log(this.People);
+  };
+
+  var b = "bob";
+  var c = "sal";
+
+  this.addPerson(b);
+  this.addPerson(c);
+
+  var d = this.People.get("c1");
+
+  d.set({name: "calvin"});
+
+  console.log(this.People);
+
+});
